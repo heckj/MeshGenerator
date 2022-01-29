a673a1bce3fce7f3325d39ca05d79d4daeded639
Joe Heck <heckj@mac.com>
2022-01-29 11:20:02 -0800

    removing DocC plugin so that CI can operate

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        // Swift-DocC Plugin
-        .package(url: "https://github.com/apple/swift-docc-plugin", branch: "main"),
+        // Swift-DocC Plugin - swift 5.6 ONLY (GitHhub Actions on 1/29/2022 only supports to 5.5)
+        //.package(url: "https://github.com/apple/swift-docc-plugin", branch: "main"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
