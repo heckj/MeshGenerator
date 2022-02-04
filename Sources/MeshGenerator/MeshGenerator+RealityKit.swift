dbdbbddb818d66e3d573926b9522b4136598abc1
Joe Heck <heckj@mac.com>
2022-02-04 14:54:00 -0800

    initial meshresource generation

--- Sources/MeshGenerator/MeshGenerator+RealityKit.swift
+++ Sources/MeshGenerator/MeshGenerator+RealityKit.swift
@@ -30,38 +30,38 @@
 #if canImport(RealityKit)
     import RealityKit
 
-    @available(macOS 12.0, iOS 15.0, *)
-    public extension Array where Element == Vertex {
-        /// Generates a mesh descriptor collection for the set of vertices that you provide
-        /// - Parameters:
-        ///   - indices: The index locations that make up the triangles associated with the collection of vertices.
-        ///   - materials: The materials associated with the vertices.
-        ///
-        ///  You can create a mesh with shared vertices and smoothed normals using generateMeshDescriptor, by providing
-        ///  a complete list of the vertices to be used, and the corresponding (typically larger) array of indices that make up the
-        ///  triangles within the mesh.
-        func generateMeshDescriptor(
-            with indices: [UInt32], materials: [UInt32] = []
-        ) -> MeshDescriptor {
-            var meshDescriptor = MeshDescriptor()
-            var positions: [SIMD3<Float>] = []
-            var normals: [SIMD3<Float>] = []
-            var uvs: [SIMD2<Float>] = []
-            for vertex in self {
-                positions.append(vertex.position.simd_float3)
-                normals.append(vertex.normal.simd_float3)
-                uvs.append(vertex.tex.simd_float2)
-            }
-            meshDescriptor.positions = MeshBuffers.Positions(positions)
-            meshDescriptor.normals = MeshBuffers.Normals(normals)
-            meshDescriptor.textureCoordinates = MeshBuffers.TextureCoordinates(uvs)
-            meshDescriptor.primitives = .triangles(indices)
-            if !materials.isEmpty {
-                meshDescriptor.materials = MeshDescriptor.Materials.perFace(materials)
-            }
-            return meshDescriptor
-        }
-    }
+//    @available(macOS 12.0, iOS 15.0, *)
+//    public extension Array where Element == Vertex {
+//        /// Generates a mesh descriptor collection for the set of vertices that you provide
+//        /// - Parameters:
+//        ///   - indices: The index locations that make up the triangles associated with the collection of vertices.
+//        ///   - materials: The materials associated with the vertices.
+//        ///
+//        ///  You can create a mesh with shared vertices and smoothed normals using generateMeshDescriptor, by providing
+//        ///  a complete list of the vertices to be used, and the corresponding (typically larger) array of indices that make up the
+//        ///  triangles within the mesh.
+//        func generateMeshDescriptor(
+//            with indices: [UInt32], materials: [UInt32] = []
+//        ) -> MeshDescriptor {
+//            var meshDescriptor = MeshDescriptor()
+//            var positions: [SIMD3<Float>] = []
+//            var normals: [SIMD3<Float>] = []
+//            var uvs: [SIMD2<Float>] = []
+//            for vertex in self {
+//                positions.append(vertex.position.simd_float3)
+//                normals.append(vertex.normal.simd_float3)
+//                uvs.append(vertex.tex.simd_float2)
+//            }
+//            meshDescriptor.positions = MeshBuffers.Positions(positions)
+//            meshDescriptor.normals = MeshBuffers.Normals(normals)
+//            meshDescriptor.textureCoordinates = MeshBuffers.TextureCoordinates(uvs)
+//            meshDescriptor.primitives = .triangles(indices)
+//            if !materials.isEmpty {
+//                meshDescriptor.materials = MeshDescriptor.Materials.perFace(materials)
+//            }
+//            return meshDescriptor
+//        }
+//    }
 
     @available(macOS 12.0, iOS 15.0, *)
     public extension Triangle {
@@ -112,7 +112,8 @@
 
         /// Returns a list of RealityKit mesh descriptors for use in generating a mesh resource.
         ///
-        /// For example:
+        /// This method returns a separate mesh descriptor for every triangle face within the mesh.
+        /// For example, create a mesh:
         /// ```
         /// let positions: [Vector] = [
         ///     Vector(x: 0.5, y: -0.4330127, z: -0.4330127), // 0
@@ -127,13 +128,128 @@
         /// let right = Triangle(positions[2], positions[1], positions[3], material: ColorRepresentation.green)
         /// let mesh = Mesh([back, bottom, left, right])
         /// ```
-        /// and then...
+        ///
+        /// Provide the list of descriptors to `MeshResource.generate` to create a mesh resource from the mesh.
         /// ```
         /// let resource = MeshResource.generate(mesh.descriptors)
+        /// ```
         var descriptors: [MeshDescriptor] {
             return polygons.map { tri in
                 tri.generateMeshDescriptor()
             }
         }
+        
+        /// A RealityKit mesh descriptor that describes the mesh as a single MeshDescriptor instance.
+        ///
+        /// For example, create a mesh:
+        /// ```
+        /// let positions: [Vector] = [
+        ///     Vector(x: 0.5, y: -0.4330127, z: -0.4330127), // 0
+        ///     Vector(x: -0.5, y: -0.4330127, z: -0.4330127), // /// 1
+        ///     Vector(x: 0, y: 0.4330127, z: 0), // 2  (peak)
+        ///     Vector(x: 0, y: -0.4330127, z: 0.4330127), // 3
+        /// ]
+        ///
+        /// let back = Triangle(positions[0], positions[1], positions[2], material: ColorRepresentation.red)
+        /// let bottom = Triangle(positions[0], positions[3], positions[1], material: ColorRepresentation.white)
+        /// let left = Triangle(positions[0], positions[2], positions[3], material: ColorRepresentation.blue)
+        /// let right = Triangle(positions[2], positions[1], positions[3], material: ColorRepresentation.green)
+        /// let mesh = Mesh([back, bottom, left, right])
+        /// ```
+        ///
+        /// Provide the descriptor to `MeshResource.generate` to create a mesh resource from the mesh.
+        /// ```
+        /// let resource = MeshResource.generate([mesh.unifiedDescriptor])
+        /// ```
+        var unifiedDescriptor: MeshDescriptor {
+            // build a complete set of the materials within the collection of triangles
+            let setOfMaterials: Set<Material> = Set(polygons.compactMap { $0.material })
+            // make a map that we can use to look up the material to generate a specific material index value
+            var materialIndexLookup: [Material:UInt32] = [:]
+            for eachMaterial in setOfMaterials {
+                materialIndexLookup[eachMaterial] = UInt32(materialIndexLookup.count)
+            }
+            
+            // flat list of all the vertices from within each triangle
+            let combined_vertices = polygons.flatMap { $0.vertices }
+
+            var meshDescriptor = MeshDescriptor()
+            var positions: [SIMD3<Float>] = []
+            var normals: [SIMD3<Float>] = []
+            var uvs: [SIMD2<Float>] = []
+            for vertex in combined_vertices {
+                positions.append(vertex.position.simd_float3)
+                normals.append(vertex.normal.simd_float3)
+                uvs.append(vertex.tex.simd_float2)
+            }
+            meshDescriptor.positions = MeshBuffers.Positions(positions)
+            meshDescriptor.normals = MeshBuffers.Normals(normals)
+            meshDescriptor.textureCoordinates = MeshBuffers.TextureCoordinates(uvs)
+            
+            // create an array of all the vertices in the order they're provided within the triangles
+            let indexArray: [UInt32] = (0..<combined_vertices.count).map { UInt32($0) }
+            meshDescriptor.primitives = .triangles(indexArray)
+
+            if materialIndexLookup.count <= 1 {
+                // If there's zero, or only one, material for the polygons within the mesh, assign the
+                // material to all faces in the mesh uniformly.
+                meshDescriptor.materials = MeshDescriptor.Materials.allFaces(0)
+            } else {
+                // If there's more than one material listed in the mesh, then we drop back to asserting
+                // a material for each polygon (triangle) face within the mesh.
+                let material_list_by_triangle = polygons.map { triangle -> UInt32 in
+                    if let material = triangle.material {
+                        if let value = materialIndexLookup[material] {
+                            return value
+                        }
+                        // if we can't look up the value (which should be impossible), or there isn't
+                        // a material listed for the triangle face, then we return a `0` index for a
+                        // default material for the whole mesh.
+                    }
+                    return 0
+                }
+                meshDescriptor.materials = MeshDescriptor.Materials.perFace(material_list_by_triangle)
+            }
+            return meshDescriptor
+        }
     }
+
+@available(macOS 12.0, iOS 15.0, *)
+extension MeshResource {
+    /// Returns a new MeshResource from the mesh instance in you provide.
+    /// - Parameter mesh: The mesh to be converted.
+    ///
+    /// For example, create a mesh:
+    /// ```
+    /// let positions: [Vector] = [
+    ///     Vector(x: 0.5, y: -0.4330127, z: -0.4330127), // 0
+    ///     Vector(x: -0.5, y: -0.4330127, z: -0.4330127), // /// 1
+    ///     Vector(x: 0, y: 0.4330127, z: 0), // 2  (peak)
+    ///     Vector(x: 0, y: -0.4330127, z: 0.4330127), // 3
+    /// ]
+    ///
+    /// let back = Triangle(positions[0], positions[1], positions[2], material: ColorRepresentation.red)
+    /// let bottom = Triangle(positions[0], positions[3], positions[1], material: ColorRepresentation.white)
+    /// let left = Triangle(positions[0], positions[2], positions[3], material: ColorRepresentation.blue)
+    /// let right = Triangle(positions[2], positions[1], positions[3], material: ColorRepresentation.green)
+    /// let mesh = Mesh([back, bottom, left, right])
+    /// ```
+    ///
+    /// Provide the created mesh to `MeshResource.generate` to create a mesh resource from the mesh.
+    /// ```
+    /// let resource = MeshResource.generate(mesh)
+    /// ```
+
+    public static func generate(mesh: Mesh) throws -> MeshResource {
+        precondition(mesh.polygons.count > 0, "Mesh must have more than one triangle in order to render.")
+        return try MeshResource.generate(from: [mesh.unifiedDescriptor])
+    }
+    
+    /// Returns a new MeshResource from the mesh instance in you provide.
+    /// - Parameter mesh: The mesh to be converted.
+    public static func generateAsync(_ mesh: Mesh) async -> LoadRequest<MeshResource> {
+        precondition(mesh.polygons.count > 0, "Mesh must have more than one triangle in order to render.")
+        return MeshResource.generateAsync(from: [mesh.unifiedDescriptor])
+    }
+}
 #endif
--- Tests/MeshGeneratorTests/RealityKitTests.swift
+++ Tests/MeshGeneratorTests/RealityKitTests.swift
@@ -45,9 +45,148 @@
                 XCTAssertEqual(resource.bounds.max.x, 0.5, accuracy: 0.0001)
                 XCTAssertEqual(resource.bounds.max.y, 0.4330127, accuracy: 0.0001)
                 XCTAssertEqual(resource.bounds.max.z, 0.4330127, accuracy: 0.0001)
-//                print(resource.contents.instances)
-//                print(resource.contents.models)
+                // print(" --- instances ---")
+                // print(resource.contents.instances)
+                /*
+                 MeshInstanceCollection(table: [
+                    "MeshModel-0": (extension in RealityFoundation):RealityKit.MeshResource.Instance(
+                        id: "MeshModel-0",
+                        model: "MeshModel",
+                        transform: simd_float4x4([[1.0, 0.0, 0.0, 0.0], [0.0, 1.0, 0.0, 0.0], [0.0, 0.0, 1.0, 0.0], [0.0, 0.0, 0.0, 1.0]])),
+                    "MeshModel[1]-0": (extension in RealityFoundation):RealityKit.MeshResource.Instance(
+                        id: "MeshModel[1]-0",
+                        model: "MeshModel[1]",
+                        transform: simd_float4x4([[1.0, 0.0, 0.0, 0.0], [0.0, 1.0, 0.0, 0.0], [0.0, 0.0, 1.0, 0.0], [0.0, 0.0, 0.0, 1.0]])),
+                    "MeshModel[3]-0": (extension in RealityFoundation):RealityKit.MeshResource.Instance(
+                        id: "MeshModel[3]-0",
+                        model: "MeshModel[3]",
+                        transform: simd_float4x4([[1.0, 0.0, 0.0, 0.0], [0.0, 1.0, 0.0, 0.0], [0.0, 0.0, 1.0, 0.0], [0.0, 0.0, 0.0, 1.0]])),
+                    "MeshModel[2]-0": (extension in RealityFoundation):RealityKit.MeshResource.Instance(
+                        id: "MeshModel[2]-0",
+                        model: "MeshModel[2]",
+                        transform: simd_float4x4([[1.0, 0.0, 0.0, 0.0], [0.0, 1.0, 0.0, 0.0], [0.0, 0.0, 1.0, 0.0], [0.0, 0.0, 0.0, 1.0]]))
+                 ])
+                 */
             }
+        
+        func testUnifiedMeshGeneration() throws {
+            let positions: [Vector] = [
+                Vector(x: 0.5, y: -0.4330127, z: -0.4330127), // 0
+                Vector(x: -0.5, y: -0.4330127, z: -0.4330127), // 1
+                Vector(x: 0, y: 0.4330127, z: 0), // 2  (peak)
+                Vector(x: 0, y: -0.4330127, z: 0.4330127), // 3
+            ]
+
+            let back = Triangle(positions[0], positions[1], positions[2], material: ColorRepresentation.red)
+            let bottom = Triangle(positions[0], positions[3], positions[1], material: ColorRepresentation.white)
+            let left = Triangle(positions[0], positions[2], positions[3], material: ColorRepresentation.blue)
+            let right = Triangle(positions[2], positions[1], positions[3], material: ColorRepresentation.green)
+            let mesh = Mesh([back, bottom, left, right])
+
+            // generating a MeshResource:
+            let resource = try MeshResource.generate(from: [mesh.unifiedDescriptor])
+            XCTAssertNotNil(resource)
+            XCTAssertEqual(resource.contents.instances.count, 1)
+            XCTAssertEqual(resource.contents.models.count, 1)
+            XCTAssertEqual(resource.expectedMaterialCount, 4)
+            print(resource.bounds)
+            XCTAssertEqual(resource.bounds.min.x, -0.5, accuracy: 0.0001)
+            XCTAssertEqual(resource.bounds.min.y, -0.4330127, accuracy: 0.0001)
+            XCTAssertEqual(resource.bounds.min.z, -0.4330127, accuracy: 0.0001)
+            XCTAssertEqual(resource.bounds.max.x, 0.5, accuracy: 0.0001)
+            XCTAssertEqual(resource.bounds.max.y, 0.4330127, accuracy: 0.0001)
+            XCTAssertEqual(resource.bounds.max.z, 0.4330127, accuracy: 0.0001)
+        }
+        
+/* example meshmodel dump:
+ 
+"MeshModel[2]": (extension in RealityFoundation):RealityKit.MeshResource.Model(
+         id: "MeshModel[2]",
+         parts: RealityFoundation.MeshPartCollection(
+            table: [
+                "MeshPart": (extension in RealityFoundation):RealityKit.MeshResource.Part(
+                    bufferDict: RealityFoundation.MeshBufferDictionary(
+                        bufferTable: [
+                            indexTriangles: RealityFoundation.MeshBufferDictionary.(unknown context at $2282367e0).BufferEntry<Swift.UInt16>(
+                                id: indexTriangles,
+                                count: 3,
+                                rate: RealityFoundation.MeshBuffers.Rate.faceVarying,
+                                elementType: RealityFoundation.MeshBuffers.ElementType.uInt16,
+                                packed: false,
+                                buffer: RealityFoundation.MeshBuffer<Swift.UInt16>(closure: RealityFoundation.MeshBuffer<Swift.UInt16>.Closures(getArray: (Function), getIndices: (Function), getData: (Function), chunk: (Function)),
+                                elementType: RealityFoundation.MeshBuffers.ElementType.uInt16,
+                                packed: false,
+                                count: 3,
+                                rate: RealityFoundation.MeshBuffers.Rate.faceVarying)
+                            ),
+                            vertexUV: RealityFoundation.MeshBufferDictionary.(unknown context at $2282367e0).BufferEntry<Swift.SIMD2<Swift.Float>>(
+                                id: vertexUV,
+                                count: 3,
+                                rate: RealityFoundation.MeshBuffers.Rate.vertex,
+                                elementType: RealityFoundation.MeshBuffers.ElementType.simd2Float,
+                                packed: false,
+                                buffer: RealityFoundation.MeshBuffer<Swift.SIMD2<Swift.Float>>(closure: RealityFoundation.MeshBuffer<Swift.SIMD2<Swift.Float>>.Closures(getArray: (Function), getIndices: (Function), getData: (Function), chunk: (Function)),
+                                elementType: RealityFoundation.MeshBuffers.ElementType.simd2Float,
+                                packed: false,
+                                count: 3,
+                                rate: RealityFoundation.MeshBuffers.Rate.vertex)
+                            ),
+                            vertexBitangent: RealityFoundation.MeshBufferDictionary.(unknown context at $2282367e0).BufferEntry<RealityFoundation.FloatVector3Packed>(
+                                id: vertexBitangent,
+                                count: 3,
+                                rate: RealityFoundation.MeshBuffers.Rate.vertex,
+                                elementType: RealityFoundation.MeshBuffers.ElementType.simd3Float,
+                                packed: true,
+                                buffer: RealityFoundation.MeshBuffer<RealityFoundation.FloatVector3Packed>(closure: RealityFoundation.MeshBuffer<RealityFoundation.FloatVector3Packed>.Closures(getArray: (Function), getIndices: (Function), getData: (Function), chunk: (Function)),
+                                elementType: RealityFoundation.MeshBuffers.ElementType.simd3Float,
+                                packed: true,
+                                count: 3,
+                                rate: RealityFoundation.MeshBuffers.Rate.vertex)
+                            ),
+                            vertexNormal: RealityFoundation.MeshBufferDictionary.(unknown context at $2282367e0).BufferEntry<RealityFoundation.FloatVector3Packed>(
+                                id: vertexNormal,
+                                count: 3,
+                                rate: RealityFoundation.MeshBuffers.Rate.vertex,
+                                elementType: RealityFoundation.MeshBuffers.ElementType.simd3Float,
+                                packed: true,
+                                buffer: RealityFoundation.MeshBuffer<RealityFoundation.FloatVector3Packed>(closure: RealityFoundation.MeshBuffer<RealityFoundation.FloatVector3Packed>.Closures(getArray: (Function), getIndices: (Function), getData: (Function), chunk: (Function)),
+                                elementType: RealityFoundation.MeshBuffers.ElementType.simd3Float,
+                                packed: true,
+                                count: 3,
+                                rate: RealityFoundation.MeshBuffers.Rate.vertex)
+                            ),
+                            vertexPosition: RealityFoundation.MeshBufferDictionary.(unknown context at $2282367e0).BufferEntry<RealityFoundation.FloatVector3Packed>(
+                                id: vertexPosition,
+                                count: 3,
+                                rate: RealityFoundation.MeshBuffers.Rate.vertex,
+                                elementType: RealityFoundation.MeshBuffers.ElementType.simd3Float,
+                                packed: true,
+                                buffer: RealityFoundation.MeshBuffer<RealityFoundation.FloatVector3Packed>(closure: RealityFoundation.MeshBuffer<RealityFoundation.FloatVector3Packed>.Closures(getArray: (Function), getIndices: (Function), getData: (Function), chunk: (Function)),
+                                elementType: RealityFoundation.MeshBuffers.ElementType.simd3Float,
+                                packed: true,
+                                count: 3,
+                                rate: RealityFoundation.MeshBuffers.Rate.vertex)
+                            ),
+                            vertexTangent: RealityFoundation.MeshBufferDictionary.(unknown context at $2282367e0).BufferEntry<RealityFoundation.FloatVector3Packed>(
+                                id: vertexTangent,
+                                count: 3,
+                                rate: RealityFoundation.MeshBuffers.Rate.vertex,
+                                elementType: RealityFoundation.MeshBuffers.ElementType.simd3Float,
+                                packed: true,
+                                buffer: RealityFoundation.MeshBuffer<RealityFoundation.FloatVector3Packed>(closure: RealityFoundation.MeshBuffer<RealityFoundation.FloatVector3Packed>.Closures(getArray: (Function), getIndices: (Function), getData: (Function), chunk: (Function)),
+                                elementType: RealityFoundation.MeshBuffers.ElementType.simd3Float,
+                                packed: true,
+                                count: 3,
+                                rate: RealityFoundation.MeshBuffers.Rate.vertex)
+                            )
+                        ]),
+                        id: "MeshPart",
+                        materialIndex: 0,
+                        materialWasInvalid: false
+                    )]
+                )
+            ),
+         */
         #endif
     }
 #endif
