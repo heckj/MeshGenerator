6f4fa32e32b318dbc932fe7af7ae2804cceb3be2
Joe Heck <heckj@mac.com>
2022-02-19 12:03:00 -0800

    script updates w/ examples and more formatting

--- docbuild.bash
+++ docbuild.bash
@@ -3,30 +3,38 @@
 echo "Make sure you've rebased over the current HEAD branch:"
 echo "git rebase -i origin/main docs"
 
-set -e
-set -x
+set -e  # exit on a non-zero return code from a command
+set -x  # print a trace of commands as they execute
 
-rm -rf .build
-mkdir -p .build/symbol-graphs
+# rm -rf .build
+# mkdir -p .build/symbol-graphs
 
-swift build --target MeshGenerator \
-    -Xswiftc -emit-symbol-graph \
-    -Xswiftc -emit-symbol-graph-dir -Xswiftc .build/symbol-graphs
+# $(xcrun --find swift) build --target MeshGenerator \
+#     -Xswiftc -emit-symbol-graph \
+#     -Xswiftc -emit-symbol-graph-dir -Xswiftc .build/symbol-graphs
 
-xcrun docc convert Sources/MeshGenerator/MeshGenerator.docc \
-    --analyze \
-    --fallback-display-name MeshGenerator \
-    --fallback-bundle-identifier com.github.heckj.MeshGenerator \
-    --fallback-bundle-version 0.1.0 \
-    --additional-symbol-graph-dir .build/symbol-graphs \
-    --experimental-documentation-coverage \
-    --level brief
-
+# Enables deterministic output 
+# - useful when you're committing the results to host on github pages
 export DOCC_JSON_PRETTYPRINT=YES
+
+# $(xcrun --find docc) convert Sources/MeshGenerator/MeshGenerator.docc \
+#     --output-path ./docs \
+#     --fallback-display-name MeshGenerator \
+#     --fallback-bundle-identifier com.github.heckj.MeshGenerator \
+#     --fallback-bundle-version 0.1.0 \
+#     --additional-symbol-graph-dir .build/symbol-graphs \
+#     --emit-digest \
+#     --transform-for-static-hosting \
+#     --hosting-base-path 'MeshGenerator'
+
+# Add the following as a dependency into your Package.swift
+#
+# // Swift-DocC Plugin - swift 5.6 ONLY (GitHhub Actions on 1/29/2022 only supports to 5.5)
+# .package(url: "https://github.com/apple/swift-docc-plugin", branch: "main"),
 
 # Swift package plugin for hosted content:
 #
-swift package \
+$(xcrun --find swift) package \
     --allow-writing-to-directory ./docs \
     --target MeshGenerator \
     generate-documentation \
@@ -36,7 +44,7 @@ swift package \
     --transform-for-static-hosting \
     --hosting-base-path 'MeshGenerator'
 
-# Generate a list of all the identifiers for DocC curation
+# Generate a list of all the identifiers to assist in DocC curation
 #
 
 cat docs/linkable-entities.json | jq '.[].referenceURL' -r > all_identifiers.txt
@@ -46,3 +54,13 @@ sort all_identifiers.txt \
     | sed -e 's/$/``/g' > all_symbols.txt
 
 echo "Page will be available at https://heckj.github.io/MeshGenerator/documentation/meshgenerator/"
+
+# xcodebuild docbuild -scheme ExampleDocC \
+#     -destination generic/platform=iOS \
+#     OTHER_DOCC_FLAGS="--transform-for-static-hosting --hosting-base-path <hosting-base-path>" \
+#     DOCC_OUTPUT_DIR=./docs
+
+# equivalent to:
+# xcodebuild docbuild -scheme ExampleDocC \
+#     -destination generic/platform=iOS \
+#     OTHER_DOCC_FLAGS="--transform-for-static-hosting --hosting-base-path ExampleDocC --output-path docs"
--- docpreview.bash
+++ docpreview.bash
@@ -1,29 +1,26 @@
 #!/bin/bash
 
-echo "Make sure you've rebased over the current HEAD branch:"
-echo "git rebase -i origin/main docs"
-
-set -e
-set -x
+set -e  # exit on a non-zero return code from a command
+set -x  # print a trace of commands as they execute
 
 rm -rf .build
 mkdir -p .build/symbol-graphs
 
-swift build --target MeshGenerator \
--Xswiftc -emit-symbol-graph \
--Xswiftc -emit-symbol-graph-dir -Xswiftc .build/symbol-graphs
+$(xcrun --find swift) build --target MeshGenerator \
+    -Xswiftc -emit-symbol-graph \
+    -Xswiftc -emit-symbol-graph-dir -Xswiftc .build/symbol-graphs
 
-xcrun docc convert Sources/MeshGenerator/MeshGenerator.docc \
---analyze \
---fallback-display-name MeshGenerator \
---fallback-bundle-identifier com.github.heckj.MeshGenerator \
---fallback-bundle-version 0.1.0 \
---additional-symbol-graph-dir .build/symbol-graphs \
---experimental-documentation-coverage \
---level brief
+$(xcrun --find docc) convert Sources/MeshGenerator/MeshGenerator.docc \
+    --analyze \
+    --fallback-display-name MeshGenerator \
+    --fallback-bundle-identifier com.github.heckj.MeshGenerator \
+    --fallback-bundle-version 0.1.0 \
+    --additional-symbol-graph-dir .build/symbol-graphs \
+    --experimental-documentation-coverage \
+    --level brief
 
-xcrun docc preview Sources/MeshGenerator/MeshGenerator.docc \
---fallback-display-name MeshGenerator \
---fallback-bundle-identifier com.github.heckj.MeshGenerator \
---fallback-bundle-version 0.1.0 \
---additional-symbol-graph-dir .build/symbol-graphs
+$(xcrun --find docc) preview Sources/MeshGenerator/MeshGenerator.docc \
+    --fallback-display-name MeshGenerator \
+    --fallback-bundle-identifier com.github.heckj.MeshGenerator \
+    --fallback-bundle-version 0.1.0 \
+    --additional-symbol-graph-dir .build/symbol-graphs
